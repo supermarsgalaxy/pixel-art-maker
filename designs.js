c18cbc75e61e75a5d20fb7ff3a6d344cea489753
Mars Brown <meeshajean33@gmail.com>
2022-03-21 16:10:46 -0400

    added comments

--- designs.js
+++ designs.js
@@ -1,10 +1,32 @@
-// Select color input
-// Select size input
+//color and size variables
+var height;
+var width;
+var color = "colorPicker";
 
+//DOM Elements
+const table = document.querySelector("#pixelCanvas");
+const pickColor = document.querySelector("#colorPicker");
+const size = document.querySelector("#sizePicker");
+//event listener
+size.addEventListener("submit", function (event) {
+  event.preventDefault();
+  width = document.getElementById("inputHeight").value;
+  height = document.getElementById("inputWidth").value;
+  makeGrid(height, width);
+});
 // When size is submitted by the user, call makeGrid()
+function makeGrid(height, width) {
+  table.innerHTML = "";
+  for (let row = 0; row < width; row++) {
+    let newRow = table.insertRow();
+    for (let cell = 0; cell < height; cell++) {
+      let newCell = newRow.insertCell();
+      newCell.onclick = changeColor;
+    }
+  }
+}
 
-function makeGrid() {
-
-// Your code goes here!
-
+//change color
+function changeColor() {
+  this.style.background = pickColor.value;
 }
